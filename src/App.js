d39df4dea058ea6f837e71dbd3836543cdaf7fd3
ChathuraSam <chathura.samarajeewa@gmail.com>
2020-01-02 01:14:33 +0530

    added 2020 by chathura

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Code from Amarok-Asia 2020
+          Code from Amarok-Asia 2020 version 2 by chathura
         </p>
         <a
           className="App-link"
